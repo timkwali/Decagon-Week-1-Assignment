25267c41710acc89abbf67a1f365e92ac3478b02
Perez Mac <apple@Timring.local>
2020-07-28 10:46:02 +0100

    initial commit

---
+++ app/src/main/java/com/example/checkpalindrome/checkPalindrome.kt
@@ -0,0 +1,27 @@
+package com.example.checkpalindrome
+
+import java.lang.Exception
+import java.util.*
+
+fun checkPalindrome(palindrome: Any): Boolean {
+    //CHECK IF PASSED ARGUMENT IS STRING
+    if(palindrome !is String)
+        return throw Exception("Argument not string, pass a string")
+    else ""
+
+//    var str = palindrome
+//    while (str != String) {
+//        println("Argument not string, input a string")
+//        str = readLine()
+//    }
+
+    //REMOVE ALL SPACE AND PUNCTUATION CHARACTERS
+    val regex = "[^A-Za-z0-9]".toRegex()
+    var string = regex.replace(palindrome, "")
+    string = string.toLowerCase()
+
+    //REVERSE STRING AND CHECK IF IT IS A PALINDROME
+    var reverseString: String = ""
+    for( i in string.length - 1 downTo 0 ) reverseString += string[i]
+    return string == reverseString
+}
